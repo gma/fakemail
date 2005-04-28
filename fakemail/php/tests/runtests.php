1983b34f1f94b47cb162d77e7ba97cfe42a322fc
pachanga <>
2005-04-28 09:45:20 +0000

    some refactoring and initial tests of FakeMailDaemon, phpDocumentor comments added

---
+++ fakemail/php/tests/runtests.php
@@ -0,0 +1,24 @@
+<?php
+    //$Id$
+    if(!defined('SIMPLE_TEST')) {
+        define('SIMPLE_TEST', 'simpletest');
+    }
+
+    require_once(SIMPLE_TEST . '/unit_tester.php');
+    require_once(SIMPLE_TEST . '/reporter.php');
+    require_once(SIMPLE_TEST . '/mock_objects.php');
+
+    class AllFakemailTests extends GroupTest {
+        function AllFakemailTests() {
+            $this->GroupTest('All tests for fakemail');
+            $this->addTestFile('fakemail_test.php');
+        }
+    }
+
+    $test =& new AllFakemailTests();
+    if (SimpleReporter::inCli()) {
+        exit ($test->run(new TextReporter()) ? 0 : 1);
+    }
+    $test->run(new HtmlReporter());
+
+?>
