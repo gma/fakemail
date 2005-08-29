e5c3b4cd07ebcbe447f628638f3667c75668db04
lastcraft <>
2005-08-29 22:04:55 +0000

    Added Grahams python version

---
+++ fakemail/python/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#
+# $Id$
+
+
+from distutils.core import *
+
+
+setup(name="fakemail-python",
+      version="1.0beta",
+      author="Graham Ashton",
+      author_email="ashtong@users.sourceforge.net",
+      url="http://fakemail.sourceforge.net/",
+      download_url="http://fakemail.sourceforge.net/",
+      scripts=["fakemail.py"],
+)
